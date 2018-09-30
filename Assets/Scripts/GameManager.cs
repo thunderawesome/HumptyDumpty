795f2695ab3c072fdbdd45e281cc32b31bbfcd87
ThunderAwesome <troyjsanders@gmail.com>
2018-09-29 20:32:38 -0500

    Cleaned up and commented code to be more clear

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -46,14 +46,13 @@ public class GameManager : MonoBehaviour
     {
         willToLiveMeterValue += value;
 
-        animator.SetFloat("Blend", willToLiveMeterValue/MAX_METER_VALUE);
+        // TJS: Blends the happy/sad face animation of the egg depending on the slider value
+        animator.SetFloat("Blend", willToLiveMeterValue / MAX_METER_VALUE);
 
-        var sfxIndex = value > 0 ? 0 : 1;
-        AudioManager.Instance.PlaySoundFX(m_camManager.audioSource, AudioManager.Instance.cameraSFX[sfxIndex]);
+        PlaySFXDependingOnPositiveOrNegativeValue(value);
 
         if (willToLiveMeterValue <= MIN_METER_VALUE)
         {
-            //TODO: Game over
             StartCoroutine(GameOver());
             return;
         }
@@ -64,18 +63,28 @@ public class GameManager : MonoBehaviour
         }
 
         willToLiveSlider.value = willToLiveMeterValue;
+        MoveTheCameraBasedOnTheMeterValue();
+    }
 
-        m_camManager.targetPosition = Vector3.Lerp(m_camManager.startingPosition, m_camManager.finalCamPosition, (MAX_METER_VALUE - willToLiveMeterValue)/MAX_METER_VALUE);
-        Debug.LogFormat("Target Position: {0}", m_camManager.targetPosition);
+    private void MoveTheCameraBasedOnTheMeterValue()
+    {
+        m_camManager.targetPosition = Vector3.Lerp(m_camManager.startingPosition, m_camManager.finalCamPosition, (MAX_METER_VALUE - willToLiveMeterValue) / MAX_METER_VALUE);
         m_camManager.StartCoroutine(m_camManager.MoveToPosition(m_mainCamera.transform, m_camManager.targetPosition, 2));
-    }    
+    }
+
+    private void PlaySFXDependingOnPositiveOrNegativeValue(float value)
+    {
+        // TJS: if the value is greater than zero then it means it is a positive thing (UP sfx)
+        var sfxIndex = value > 0 ? 0 : 1;
+        AudioManager.Instance.PlaySoundFX(m_camManager.audioSource, AudioManager.Instance.cameraSFX[sfxIndex]);
+    }
 
     private IEnumerator GameOver()
     {
         willToLiveMeterValue = MIN_METER_VALUE;
         willToLiveSlider.value = willToLiveMeterValue;
 
-        yield return null; //TODO: 
+        yield return null; //TODO: Fade to black
     }
 
     #endregion
